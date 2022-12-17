b1c4f423ad403fa36f1ec5e82bc7c1cb11d8a328
alex <alex@slab.org>
2022-12-17 17:14:17 +0000

    tweaks

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -32,7 +32,7 @@ function EventHeader({event, reservedStreamCount, totalStreamCount}:Props): Reac
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h4>Tidal Club Presents ...</h4>
-            <h1>The Longest Night</h1>
+            <h1>Night Stream</h1>
             <h2> {event && DateTime.fromISO(event.starts_at).toFormat("dd. LLLL HH:mm")} - {event && DateTime.fromISO(event.ends_at).toFormat("dd. LLLL HH:mm")} {event && DateTime.fromISO(event.starts_at).toFormat("yyyy")}</h2>
             <hr/>
 
